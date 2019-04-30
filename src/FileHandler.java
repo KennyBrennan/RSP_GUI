f3db52ac67568fc6ba651a7437b7043913dbe5bf
CJ <cdevito@oswego.edu>
2019-04-30 13:35:38 -0400

    ./re working

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -161,8 +161,8 @@ public class FileHandler {
         Scanner in;
         historyData = new LinkedList();
         try {
-            history = new File("history.data"); //todo: Update to real file path
-           // history = new File(directory.toString() + "/LOGS/history.data");
+           // history = new File("history.data"); //todo: Update to real file path
+            history = new File(directory.toString() + "/LOGS/history.data");
             System.out.println("\n\nhistory = " + history);
             in = new Scanner(history);
             for (int i = 0; i < 81; i++) {
